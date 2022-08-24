742d220cdcdb171e751ca07008bd1e17d2dc7ece
EliuTovar <99421840+EliuTovar@users.noreply.github.com>
2022-08-24 16:45:28 -0500

    Add files via upload

---
+++ Eva1_Practica6_NanoTime/src/eva1_practica6_nanotime/Eva1_Practica6_NanoTime.java
@@ -0,0 +1,12 @@
+
+package eva1_practica6_nanotime;
+
+
+public class Eva1_Practica6_NanoTime {
+
+    
+    public static void main(String[] args) {
+        System.out.println(System.nanoTime());
+    }
+    
+}
