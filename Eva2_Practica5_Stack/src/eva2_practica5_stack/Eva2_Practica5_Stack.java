fc344ea6090a8f867ecfbf3d6d13e99c81791afa
EliuTovar <99421840+EliuTovar@users.noreply.github.com>
2022-11-09 13:59:14 -0600

    Add files via upload

---
+++ Eva2_Practica5_Stack/src/eva2_practica5_stack/Eva2_Practica5_Stack.java
@@ -0,0 +1,12 @@
+
+package eva2_practica5_stack;
+
+
+public class Eva2_Practica5_Stack {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Eva2_Practica5_Stack/src/eva2_practica5_stack/Stack.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_practica5_stack;
+
+/**
+ *
+ * @author tovar
+ */
+public class Stack {
+    
+    
+    
+}
